92b62023b744b232b7b234a27d8534f8d2f440f4
anhpdhe171578 <114935706+anhpdhe171578@users.noreply.github.com>
2022-11-08 17:35:14 +0700

    khoi phuc atteden

--- Assignment_PRJ301/src/java/controller/AttendenController.java
+++ Assignment_PRJ301/src/java/controller/AttendenController.java
@@ -21,33 +21,33 @@ import model.Student;
 public class AttendenController extends HttpServlet{
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        int sesid = Integer.parseInt(request.getParameter("id"));
-        SessionDBContext sesDB = new SessionDBContext();
-        Session ses = sesDB.get(sesid);
-        request.setAttribute("ses", ses);
-        request.getRequestDispatcher("../view/lecturer/Attenden.jsp").forward(request, response);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Session ses = new Session();
-        ses.setId(Integer.parseInt(request.getParameter("sesid")));
-        String [] stdids = request.getParameterValues("stdid");
+        ses.setId(Integer.parseInt(req.getParameter("sesid")));
+        String [] stdids = req.getParameterValues("stdid");
         for (String stdid : stdids) {
             Attandance a = new Attandance();
             Student s = new Student();
             a.setStudent(s);
-            a.setDescription(request.getParameter("description"+stdid));
-            a.setPresent(request.getParameter("present"+stdid).equals("present"));
+            a.setDescription(req.getParameter("description"+stdid));
+            a.setPresent(req.getParameter("present"+stdid).equals("present"));
             s.setId(Integer.parseInt(stdid));
             ses.getAttandances().add(a);
         }
         SessionDBContext sdb = new SessionDBContext();
         sdb.update(ses);
-        response.sendRedirect("takeatt?id="+ses.getId());
+        resp.sendRedirect("takeatt?id="+ses.getId());
     }
-    
-    
-    
-}
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int sesid = Integer.parseInt(req.getParameter("id"));
+        SessionDBContext sesDB = new SessionDBContext();
+        Session ses = sesDB.get(sesid);
+        req.setAttribute("ses", ses);
+        req.getRequestDispatcher("../view/lecturer/Attenden.jsp").forward(req, resp);
+    }
+    }
+
+
+
